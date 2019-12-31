9fc7b70a704ac72c2cef6f07b48a2b88a6cc6ada
Matthew Knudsen <mknudsen01@gmail.com>
2019-12-30 20:00:43 -0800

    import modules into index file

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import CenteredOverlay from './CenteredOverlay'
+import mediaqueries from './mediaqueries'
+import Text from './Text'
+
+export default {
+  CenteredOverlay,
+  mediaqueries,
+  Text
+}
