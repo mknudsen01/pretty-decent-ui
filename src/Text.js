4632cd31017e91f9ac7d9567b41bd7c774364343
Matthew Knudsen <mknudsen01@gmail.com>
2019-12-30 19:57:43 -0800

    Copy over text, mediaqueries, and centered overlay

---
+++ src/Text.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+import mediaqueries from './mediaqueries';
+
+const Text = styled.div`
+  font-size: ${props => props.size ? props.theme.typography.sizes[props.size] : props.theme.typography.sizes['base']};
+  line-height: ${props => props.lineHeight || 'inherit'};
+  color: ${props => props.variant ? props.theme.colors[props.variant] : 'inherit'};
+  text-align: ${props => props.align || 'left'};
+  font-weight: ${props => props.fontWeight || 'normal'};
+  text-decoration: ${props => props.textDecoration || 'none'};
+  letter-spacing: ${props => props.letterSpacing ? `${props.letterSpacing}px` : 'normal'};
+
+  ${mediaqueries.xs`
+    font-size: ${props => props.theme.typography.sizes[props.xs || props.size]};
+  `}
+  ${mediaqueries.sm`
+    font-size: ${props => props.theme.typography.sizes[props.sm || props.xs || props.size]};
+  `}
+  ${mediaqueries.md`
+    font-size: ${props => props.theme.typography.sizes[props.md || props.sm || props.xs || props.size]};
+  `}
+  ${mediaqueries.lg`
+    font-size: ${props => props.theme.typography.sizes[props.lg || props.md || props.sm || props.xs || props.size]};
+  `}
+`
+
+export default Text;
---
+++ src/mediaqueries.js
@@ -0,0 +1,16 @@
+import { css } from 'styled-components';
+
+const breakpoints = { xs: 0, sm: 600, md: 960, lg: 1280, xl: 1920 };
+
+// Iterate through the sizes and create a media template
+const mediaqueries = Object.keys(breakpoints).reduce((acc, label) => {
+  acc[label] = (...args) => css`
+    @media (min-width: ${breakpoints[label] / 16}em) {
+      ${css(...args)}
+    }
+  `;
+
+  return acc;
+}, {});
+
+export default mediaqueries;
