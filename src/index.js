0afdd341e283d88a9af85f6a9671d38d30e687e7
Matthew Knudsen <mknudsen01@gmail.com>
2019-12-30 20:34:23 -0800

    see if can export consts in index file

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import CenteredOverlay from './CenteredOverlay'
 import mediaqueries from './mediaqueries'
 import Text from './Text'
 
+
+export const Text;
+export const CenteredOverlay;
+export const mediaqueries;
+
 export default {
   CenteredOverlay,
   mediaqueries,
