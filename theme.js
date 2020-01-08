2d3711815ad6b180b2324c20864525712ec78fcf
Matthew Knudsen <mknudsen01@gmail.com>
2020-01-08 10:55:03 -0800

    BREAKING: change primary and secondary color palettes to be objects with shades

---
+++ theme.js
@@ -0,0 +1,72 @@
+export default {
+  fontFamily: {
+    serif: 'Georgia, Cambria, "Times New Roman", Times, serif',
+    "sans-serif": '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";',
+    mono: 'Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace'
+  },
+  maxContentWidth: 1080,
+  defaultFontFamilyVariant: 'sans-serif',
+  colors: {
+    defaultText: "#081e3e",
+    textLight: "#d9efff",
+    white: '#fff',
+    black: "#111",
+    white: {
+      500: '#fff'
+    },
+    black: {
+      500: '#111'
+    },
+    primary: {
+      500: '#146FB0',
+      700: '#105784'
+    },
+    secondary: {
+      500: '#BF363E',
+      700: '#942933'
+    },
+    text: {
+      300: "#d9efff",
+      500: "#081e3e",
+    },
+    gray: {
+      100: '#F7FAFC',
+      700: '#4a5568'
+    }
+  },
+  typography: {
+    sizes: {
+      xs: '.75rem',
+      sm: '.875rem',
+      base: '1rem',
+      lg: '1.125rem',
+      xl: '1.25rem',
+      '2xl': '1.5rem',
+      '3xl': '1.875rem',
+      '4xl': '2.25rem',
+      '5xl': '3rem',
+      '6xl': '4rem',
+    },
+  },
+  spacing: {
+    px: '1px',
+    '0': '0',
+    '1': '0.25rem',
+    '2': '0.5rem',
+    '3': '0.75rem',
+    '4': '1rem',
+    '5': '1.25rem',
+    '6': '1.5rem',
+    '8': '2rem',
+    '10': '2.5rem',
+    '12': '3rem',
+    '16': '4rem',
+    '20': '5rem',
+    '24': '6rem',
+    '32': '8rem',
+    '40': '10rem',
+    '48': '12rem',
+    '56': '14rem',
+    '64': '16rem',
+  }
+}
